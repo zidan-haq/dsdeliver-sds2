c4a6834e778882aa2ffe7d8e3652b03985974ea9
null <zidanaraujohaq@gmail.com>
2021-01-07 23:06:10 -0300

    Add Routes and StepsHeader

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,14 +1,14 @@
 import { useEffect } from 'react';
 import './App.css';
 import Navbar from './Navbar';
-import Home from './Home'
+import Routes from './Routes';
+
 
 function App() {
 
   return (
     <>
-      <Navbar />
-      <Home />
+      <Routes />
     </>
   );
 }
