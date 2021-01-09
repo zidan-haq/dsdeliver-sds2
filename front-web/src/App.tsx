47a24e58a3d0ed1ce232b3b520b972220990fe2e
null <zidanaraujohaq@gmail.com>
2021-01-09 16:13:13 -0300

    Finish front-web

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,6 +1,6 @@
-import { useEffect } from 'react';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import './App.css';
-import Navbar from './Navbar';
 import Routes from './Routes';
 
 
@@ -9,6 +9,7 @@ function App() {
   return (
     <>
       <Routes />
+      <ToastContainer />
     </>
   );
 }
