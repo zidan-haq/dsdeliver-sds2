016621d915868b973abb4dfad981057f9a377de6
null <zidanaraujohaq@gmail.com>
2021-01-10 12:47:20 -0300

    Add orders list

--- front-mobile/src/Home/index.tsx
+++ front-mobile/src/Home/index.tsx
@@ -1,15 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
+import { useNavigation } from '@react-navigation/native';
 import React from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
 import { RectButton } from 'react-native-gesture-handler';
+import Header from '../Header';
 
 function Home() {
-    const handlePress = () => {
+    const navigation = useNavigation();
 
+    const handlePress = () => {
+        navigation.navigate('Orders');
     }
 
     return (
         <>
+        <Header />
             <View style={styles.container}>
                 <Image source={require('../assets/deliveryman.png')} />
                 <Text style={styles.title}>
